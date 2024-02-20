b2b924732488771ca25b25cf82d567f86c17964f
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-20 09:22:57 -0600

    use a tooltip component

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Hint } from "@/components/hint"
 import { cn } from "@/lib/utils"
 import { useOrganization, useOrganizationList } from "@clerk/nextjs"
 import Image from "next/image"
@@ -24,16 +25,23 @@ export const Item = ({id, name, imageUrl}: ItemProps) => {
     }
     return (
         <div className="aspect-square relative">
-            <Image
-                src={imageUrl}
-                alt={name}
-                fill
-                onClick={handleClick}
-                className={cn(
-                    "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-                    isActive && "opacity-100"
-                )}
-            />
+            <Hint 
+                label={name}
+                side="right"
+                align="start"
+                sideOffset={18}
+            >
+                <Image
+                    src={imageUrl}
+                    alt={name}
+                    fill
+                    onClick={handleClick}
+                    className={cn(
+                        "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+                        isActive && "opacity-100"
+                    )}
+                />
+            </Hint>
         </div>
     )
 }

0bd80d09993f42f73ff8e865088cdbcdbac44ff8
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-20 14:58:25 -0600

    add restore point

--- convex/board.ts
+++ convex/board.ts
@@ -42,3 +42,45 @@ export const create = mutation({
     return board;
   },
 });
+
+export const restore = mutation({
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+    imageUrl: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const board = await ctx.db.insert("boards", {
+      title: args.title,
+      orgId: args.orgId,
+      authorId: identity.subject,
+      authorName: identity.name!,
+      imageUrl: args.imageUrl,
+    });
+
+    return board;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id('boards'),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: check to delete favorite reation as well
+
+    await ctx.db.delete(args.id);
+  },
+});

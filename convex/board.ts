a2c7c623bfe6d560b6c9920f61a663d262072f30
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-20 22:07:56 -0600

    verify if is favorite when board is removed

--- convex/board.ts
+++ convex/board.ts
@@ -108,7 +108,20 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: check to delete favorite reation as well
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex('by_user_board', (q) => 
+        q
+          .eq('userId', userId)
+          .eq('boardId', args.id)
+      )
+      .unique()
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
   },
@@ -136,11 +149,10 @@ export const favorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex('by_user_board_org', (q) => 
+      .withIndex('by_user_board', (q) => 
         q
           .eq('userId', userId)
           .eq('boardId', board._id)
-          .eq('orgId', board.orgId)
       )
       .unique()
 
@@ -183,7 +195,6 @@ export const unfavorite = mutation({
         q
           .eq('userId', userId)
           .eq('boardId', board._id)
-          //TODO: check if orgId is needed
       )
       .unique()
 

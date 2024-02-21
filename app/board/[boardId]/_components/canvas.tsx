1ebb5338f78ba052bf3a758cdba5fff6d0a3cf9a
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-20 23:38:40 -0600

    add structure controls to canva

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { Info } from "./info"
+import { Participants } from "./participants"
+import { Toolbar } from "./toolbar"
 
 export const Canvas = () => {
     return (
@@ -8,6 +10,8 @@ export const Canvas = () => {
             className="h-full w-full relative bg-neutral-100 touch-none"
         >
             <Info/>
+            <Participants/>
+            <Toolbar/>
         </main>
     )
 }

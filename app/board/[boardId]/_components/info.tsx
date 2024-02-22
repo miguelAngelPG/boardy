60e850b0fd421add1261481e764432841201b6ea
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-22 00:29:47 -0600

    add logo, name app and board name button to rename board

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useRenameModal } from "@/store/use-rename-modal";
 import { useQuery } from "convex/react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 interface InfoProps {
     boardId: string;
@@ -16,21 +20,49 @@ const font = Poppins({
     weight: ['600'],
 })
 
+const TapSeparator = () => {
+    return (
+        // <div className="w-1 h-8 bg-gray-300 mx-2"/>
+        <div className="text-neutral-300 px-1.5">|</div>
+    )
+}
+
 export const Info = ({ boardId }: InfoProps) => {
     const data = useQuery(api.board.get, { id: boardId as Id<'boards'> });
     
+    const { onOpen, onClose } = useRenameModal();
+
     if (!data) return <InfoSkeleton/>
 
     return (
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md"> 
-            <Button className="px-2">
-                <Image
-                    src='/logo.png'
-                    alt="Boardy Logo"
-                    height={40}
-                    width={40}
-                />
-            </Button>
+            <Hint label="Go to boards" side="bottom" sideOffset={10}>
+                <Button asChild variant='board' className="px-2">
+                    <Link href='/'>
+                        <Image
+                            src='/logo.png'
+                            alt="Boardy Logo"
+                            height={40}
+                            width={40}
+                            className="invert"
+                        />
+                        <span className={cn(
+                            'font-semibold text-xl ml-2 text-black',
+                            font.className
+                        )}>Boardy</span>
+                    </Link>
+                </Button>
+            </Hint>
+            <TapSeparator/>
+            <Hint label="Rename board" side="bottom" sideOffset={10}>
+                <Button 
+                    variant='board' 
+                    className="text-base, font-normal px-2" 
+                    onClick={() => onOpen(data._id, data.title)}
+                >
+                    { data.title }
+                </Button>   
+            </Hint>
         </div>
     )
 }

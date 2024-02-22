ac263246bfa2e89117b0887dff715a2fec3fcd5d
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-22 08:52:14 -0600

    create undo and redo functions and state

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { use, useState } from "react";
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
+import { CanvasMode, canvasState } from "@/types/canvas";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
 
 interface CanvasProps {
     boardId: string;
@@ -10,13 +13,28 @@ interface CanvasProps {
 
 export const Canvas = ({ boardId }: CanvasProps) => {
 
+    const [canvasSate, setCanvasSate] = useState<canvasState>({
+        mode: CanvasMode.None
+    })
+
+    const history = useHistory()
+    const canUndo = useCanUndo()
+    const canRedo = useCanRedo() 
+
     return (
         <main
             className="h-full w-full relative bg-neutral-100 touch-none"
         >
             <Info boardId={boardId}/>
             <Participants/>
-            <Toolbar/>
+            <Toolbar 
+                canvasState={canvasSate}
+                setCanvasState={setCanvasSate}
+                canRedo={ canRedo }
+                canUndo={ canUndo }
+                undo={ history.undo }
+                redo={ history.redo }
+            />
         </main>
     )
 }
